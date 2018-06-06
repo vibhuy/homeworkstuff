57de32a177ecd118a5da459078eb5f81fcd60a47
vibhu253 <36526050+vibhu253@users.noreply.github.com>
2018-06-06 14:39:14 -0500

    Tweet Analysis

---
+++ SocialMediaAnalytics/config.py
@@ -0,0 +1,4 @@
+consumer_key = "U3lBHco6XVkc30B5i3HMzsP5r"
+consumer_secret = "9xHDxhBoUpUE64snsRypNendoBqco77OjWN98u43vPhHB3mAbG"
+access_token = "998374637087219712-cVKJoIKqBjQUhXBZAHzvOAVaDVwNAIF"
+access_token_secret = "dSGN6RhiV6SxoAHcQYkXGoqJ8WuYR0xnaarG935vHeCi4"
