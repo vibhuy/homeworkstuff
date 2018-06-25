ff15626558969c87cd04c17cf8dba841c38a1a50
vibhu253 <36526050+vibhu253@users.noreply.github.com>
2018-06-24 22:28:44 -0500

    SQL Homework

--- SQL_Homework/SQL_Homework.sql
+++ SQL_Homework/SQL_Homework.sql
@@ -82,7 +82,8 @@ WHERE
 -- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO. Otherwise, change the first name to MUCHO GROUCHO, as that is exactly what the actor will be with the grievous error. BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY ACTOR TO MUCHO GROUCHO, HOWEVER! (Hint: update the record using a unique identifier.)
  UPDATE actor 
 SET 
-    first_name = CASE
+    first_name = 
+		CASE
         WHEN first_name = 'HARPO' THEN 'GROUCHO'
         ELSE 'MUCHO GROUCHO'
     END
